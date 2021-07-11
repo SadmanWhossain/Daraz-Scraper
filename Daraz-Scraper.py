08b73bf71893ee98e546a54d5dddedfc7c50f14d
Sadmanhssn <72847790+Sadmanhssn@users.noreply.github.com>
2021-07-11 20:40:49 +0600

    website scrape
    enter website
    search keyword

--- Daraz-Scraper.py
+++ Daraz-Scraper.py
@@ -1,10 +1,11 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'Daraz-Scraper.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost!
+from selenium import webdriver
+from openpyxl import workbook, load_workbook
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+import os
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
@@ -36,6 +37,35 @@ class Ui_DarazScraper(object):
         DarazScraper.setWindowTitle(_translate("DarazScraper", "Daraz Scraper"))
         self.label.setText(_translate("DarazScraper", "Search Keyword"))
         self.pushButton.setText(_translate("DarazScraper", "Collect"))
+
+class scraping:
+    def execution(self, keyword):
+        wb = load_workbook(keyword + '.xlsx')
+
+        ws = wb[keyword]
+        # ws = wb.create_sheet("vans")
+
+        # print(wb.sheetnames)
+        path = "G:/Project/Daraz-Scraper/chromedriver.exe"
+        driver = webdriver.Chrome(path)
+
+        fake_name_websites = driver.get("https://www.daraz.com.bd/")
+
+        searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='search']")))
+        searchbox.clear()
+        k = "sunglass"
+        searchbox.send_keys(k)
+        searchbutton = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'SEARCH')]"))).click()
+
+        links = []
+
+        products = driver.find_elements_by_xpath("//div[@data-tracking='product-card']//a")
+        for product in products:
+            links.append(product.get_property('href'))
+        print(len(links))
+        print(links)
+
+
 
 
 if __name__ == "__main__":
